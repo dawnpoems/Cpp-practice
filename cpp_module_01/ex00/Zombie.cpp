6761451babf7db3e4f198f589b83993700a80437
Yegeun Kim <yegkim@c5r7s7.42seoul.kr>
2024-06-11 19:28:00 +0900

    module_01 first draft

---
+++ cpp_module_01/ex00/Zombie.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Zombie.hpp"
+
+Zombie::Zombie()
+{
+}
+
+Zombie::Zombie(std::string name) : name(name)
+{
+}
+
+Zombie::~Zombie()
+{
+}
+
+void Zombie::announce(void)
+{
+	std::cout << this->name << " BraiiiiiiinnnzzzZ..." << std::endl;
+}
