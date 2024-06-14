1465f70af3a4dd09ccafd7570802107d09d11174
Yegeun Kim <yegkim@c5r7s7.42seoul.kr>
2024-06-14 15:33:20 +0900

    feat:add 01-00 constructor destructor log and main

--- cpp_module_01/ex00/Zombie.cpp
+++ cpp_module_01/ex00/Zombie.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include "Zombie.hpp"
 
-Zombie::Zombie()
-{
-}
-
 Zombie::Zombie(std::string name) : name(name)
 {
+	std::cout << this->name << " is born." << std::endl;
 }
 
 Zombie::~Zombie()
 {
+	std::cout << this->name << " is dead." << std::endl;
 }
 
 void Zombie::announce(void)
