b839fd9eb23e9e5f661e3acb6a87c4be7a87c60e
Yegeun Kim <yegkim@c5r7s7.42seoul.kr>
2024-06-18 13:59:29 +0900

    2-2 delete log made main

--- cpp_module_02/ex02/main.cpp
+++ cpp_module_02/ex02/main.cpp
@@ -5,12 +5,34 @@ int main( void ) {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout <<"1 > 2 :" << (Fixed(1) > Fixed(2)) << std::endl;
+	std::cout <<"1 < 2 :" << (Fixed(1) < Fixed(2)) << std::endl;
+	std::cout <<"1 >= 2 :" << (Fixed(1) >= Fixed(2)) << std::endl;
+	std::cout <<"1 >= 1 :" << (Fixed(1) >= Fixed(1)) << std::endl;
+	std::cout <<"1 <= 2 :" << (Fixed(1) <= Fixed(2)) << std::endl;
+	std::cout <<"1 <= 1 :" << (Fixed(1) <= Fixed(1)) << std::endl;
+	std::cout <<"1 == 2 :" << (Fixed(1) == Fixed(2)) << std::endl;
+	std::cout <<"1 == 1 :" << (Fixed(1) == Fixed(1)) << std::endl;
+	std::cout <<"1 != 2 :" << (Fixed(1) != Fixed(2)) << std::endl;
+	std::cout <<"1 != 1 :" << (Fixed(1) != Fixed(1)) << std::endl;
+
+	std::cout <<"1 + 2 :" << (Fixed(1) + Fixed(2)) << std::endl;
+	std::cout <<"1 - 2 :" << (Fixed(1) - Fixed(2)) << std::endl;
+	std::cout <<"1 * 2 :" << (Fixed(1) * Fixed(2)) << std::endl;
+	std::cout <<"1 / 2 :" << (Fixed(1) / Fixed(2)) << std::endl;
+
+	std::cout << "a : " << a << std::endl;
+	std::cout << "++a : " << ++a << std::endl;
+	std::cout << "a : " << a << std::endl;
+	std::cout << "a++ :" << a++ << std::endl;
+	std::cout << "a : " << a << std::endl;
+	std::cout << "--a : " << --a << std::endl;
+	std::cout << "a : " << a << std::endl;
+	std::cout << "a-- : " << a-- << std::endl;
+	std::cout << "a : " << a << std::endl;
+	std::cout << "b : " << b << std::endl;
+	std::cout << "max(a, b) : " << Fixed::max( a, b ) << std::endl;
+	std::cout << "min(a, b) : " << Fixed::min( a, b ) << std::endl;
+
 	return 0;
 }
